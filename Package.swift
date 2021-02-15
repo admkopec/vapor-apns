739c22363f52a2552affbe38e2fecfd6d2030ba3
Adam Kopeć <adamkopec5@icloud.com>
2021-02-15 11:35:02 +0100

    Updated dependency list

--- Package.swift
+++ Package.swift
@@ -7,12 +7,13 @@ let package = Package(
         .library(name: "VaporAPNS", targets: ["VaporAPNS"])
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
         .package(url: "https://github.com/toto/CCurl.git", from: "0.4.0")
     ],
     targets: [
-        .target(name: "VaporAPNS", dependencies: ["CCurl", "Console", "JWT"]),
+        .target(name: "VaporAPNS", dependencies: ["CCurl", "Console", "Service", "JWT"]),
         .testTarget(name: "VaporAPNSTests", dependencies: ["VaporAPNS"])
     ]
 )
